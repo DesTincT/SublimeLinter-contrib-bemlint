7681fd1ebb5a43073c8972069b8a8b176c60e92d
Иван Соболев <ivan.sobolev@reg.ru>
2017-09-13 20:53:02 +0400

    travis

--- linter.py
+++ linter.py
@@ -17,18 +17,18 @@ from SublimeLinter.lint import NodeLinter, util
 class Bemlint(NodeLinter):
     """Provides an interface to bemlint."""
 
-    syntax              = ('html', 'html+tt2', 'html+tt3')
-    cmd                 = ('bemlint', '@', '--format', 'compact')
-    version_args        = '--version'
-    version_re          = r'v(?P<version>\d+\.\d+\.\d+)'
+    syntax = ('html', 'html+tt2', 'html+tt3')
+    cmd = ('bemlint', '@', '--format', 'compact')
+    version_args = '--version'
+    version_re = r'v(?P<version>\d+\.\d+\.\d+)'
     version_requirement = '>= 1.4.5'
-    config_file         = ('--config', '.bemlint.json')
+    config_file = ('--config', '.bemlint.json')
     regex = (
         r'^.+?: line (?P<line>\d+), col (?P<col>\d+), '
         r'(?:(?P<error>Error)|(?P<warning>Warning)) - '
         r'(?P<message>.+)'
     )
-    multiline       = False
-    line_col_base   = (1, 1)
-    error_stream    = util.STREAM_BOTH
+    multiline = False
+    line_col_base = (1, 1)
+    error_stream = util.STREAM_BOTH
     tempfile_suffix = 'bem'
